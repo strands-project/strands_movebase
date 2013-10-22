808bbeac9fdcb706753877528b42973b08956b43
Nils Bore <nbore@kth.se>
2013-10-22 15:19:28 +0200

    Added chest calibratioN

---
+++ src/calibrate_chest.cpp
@@ -0,0 +1,138 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <boost/thread/thread.hpp>
+
+bool is_inlier(const Eigen::Vector3f& point, const Eigen::Vector4f plane, double threshold)
+{
+    return fabs(point.dot(plane.segment<3>(0)) + plane(3)) < threshold;
+}
+
+void plot_best_plane(const pcl::PointCloud<pcl::PointXYZ>& points, const Eigen::Vector4f plane, double threshold)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inlier_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr outlier_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    
+    for (int i = 0; i < points.size(); ++i) {
+        if (is_inlier(points[i].getVector3fMap(), plane, threshold)) {
+            inlier_cloud->push_back(points[i]);
+        }
+        else {
+            outlier_cloud->push_back(points[i]);
+        }
+    }
+    
+    pcl::visualization::PCLVisualizer viewer("3D Viewer");
+    viewer.setBackgroundColor(0, 0, 0);
+    viewer.addCoordinateSystem(1.0);
+    viewer.initCameraParameters();
+    
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> inlier_color_handler(inlier_cloud, 255, 0, 0);
+    viewer.addPointCloud(inlier_cloud, inlier_color_handler, "inliers");
+    
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> outlier_color_handler(outlier_cloud, 0, 0, 255);
+    viewer.addPointCloud(outlier_cloud, outlier_color_handler, "outliers");
+    
+    while (!viewer.wasStopped())
+    {
+        viewer.spinOnce(100);
+        boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+    }
+    
+}
+
+void compute_plane(Eigen::Vector4f& plane, const pcl::PointCloud<pcl::PointXYZ>& points, int* inds)
+{
+    Eigen::Vector3f first = points[inds[1]].getVector3fMap() - points[inds[0]].getVector3fMap();
+    Eigen::Vector3f second = points[inds[2]].getVector3fMap() - points[inds[0]].getVector3fMap();
+    Eigen::Vector3f normal = first.cross(second);
+    normal.normalize();
+    plane.segment<3>(0) = normal;
+    plane(3) = -normal.dot(points[inds[0]].getVector3fMap());
+}
+
+void extract_height_and_angle(const Eigen::Vector4f& plane)
+{
+    ROS_INFO("Ground plane: %f, %f, %f, %f", plane(0), plane(1), plane(2), plane(3));
+    double dist = fabs(plane(3)/plane(2)); // distance along z axis
+    double height = fabs(plane(3)/plane.segment<3>(0).squaredNorm()); // height
+    ROS_INFO("Distance to plane along camera axis: %f", dist);
+    ROS_INFO("Height above ground: %f", height);
+    double angle = -asin(height/dist);
+    ROS_INFO("Angle radians: %f", angle);
+    ROS_INFO("Angle degrees: %f", 180.0f*angle/M_PI);
+}
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    ROS_INFO("Got a pointcloud, calibrating...");
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    pcl::fromROSMsg(*msg, cloud);
+    
+    int nbr = cloud.size();
+    
+    int max = 1000;
+    double threshold = 0.02;
+    
+    Eigen::Vector4f best_plane;
+    int best_inliers = -1;
+    
+    int inds[3];
+    
+    Eigen::Vector4f plane;
+    int inliers;
+    for (int i = 0; i < max; ++i) {
+        
+        for (int j = 0; j < 3; ++j) {
+            inds[j] = rand() % nbr;
+        }
+        
+        if (inds[0] == inds[1] || inds[0] == inds[2] || inds[1] == inds[2]) {
+            continue;
+        }
+        
+        compute_plane(plane, cloud, inds);
+        inliers = 0;
+        for (int j = 0; j < nbr; j += 30) {
+            if (is_inlier(cloud[j].getVector3fMap(), plane, threshold)) {
+                ++inliers;
+            }
+        }
+        
+        if (inliers > best_inliers) {
+            best_plane = plane;
+            best_inliers = inliers;
+        }
+    }
+    
+    extract_height_and_angle(best_plane);
+    plot_best_plane(cloud, best_plane, threshold);
+    
+    exit(0);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "calibrate_chest");
+	ros::NodeHandle n;
+	
+	/*
+    // topic of the depth and rgb images
+    if (!n.hasParam("/image_player_node/camera_topic")) {
+        ROS_ERROR("Could not find parameter camera_topic.");
+        return -1;
+    }
+    std::string camera_topic;
+    n.getParam("/image_player_node/camera_topic", camera_topic);
+    */
+    std::string camera_topic = "head_xtion";
+    
+	ros::Subscriber sub = n.subscribe(camera_topic + "/depth/points", 1, callback);
+    
+    ros::spin();
+	
+	return 0;
+}
