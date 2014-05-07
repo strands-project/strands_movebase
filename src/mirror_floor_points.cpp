014fd8858406e740ae21c4949175b0e51fb6bf6e
Nils Bore <nbore@kth.se>
2014-05-07 12:06:37 +0200

    Added first version of node that looks up camera transform and mirros negative points towards camera

---
+++ src/mirror_floor_points.cpp
@@ -0,0 +1,97 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/thread/thread.hpp>
+#include <tf/transform_listener.h>
+#include <Eigen/Dense>
+
+ros::Publisher pub;
+
+double height;
+Eigen::Vector3d normal;
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::fromROSMsg(*msg, *cloud);
+	
+	for (size_t i = 0; i < cloud->size(); ++i) {
+	    
+	}
+
+	sensor_msgs::PointCloud2 msg_cloud;
+    pcl::toROSMsg(voxel_cloud, msg_cloud);
+	msg_cloud.header.frame_id = msg->header.frame_id;
+
+	pub.publish(msg_cloud);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "subsample_cloud");
+	ros::NodeHandle n;
+	
+	ros::NodeHandle pn("~");
+    // topic of input cloud
+    if (!pn.hasParam("input")) {
+        ROS_ERROR("Could not find parameter input.");
+        return -1;
+    }
+    std::string input;
+    pn.getParam("input", input);
+    
+    // topic of output cloud
+    if (!pn.hasParam("obstacle_output")) {
+        ROS_ERROR("Could not find parameter obstacle_output.");
+        return -1;
+    }
+    std::string obstacle_output;
+    pn.getParam("obstacle_output", obstacle_output);
+    
+    // topic of output cloud
+    if (!pn.hasParam("camera_frame")) {
+        ROS_ERROR("Could not find parameter camera_frame.");
+        return -1;
+    }
+    std::string camera_frame;
+    pn.getParam("camera_frame", camera_frame);
+    
+    // topic of output cloud
+    if (!pn.hasParam("floor_output")) {
+        ROS_ERROR("Could not find parameter floor_output.");
+        return -1;
+    }
+    std::string floor_output;
+    pn.getParam("floor_output", floor_output);
+    
+	ros::Subscriber sub = n.subscribe(input, 1, callback);
+    pub = n.advertise<sensor_msgs::PointCloud2>(output, 1);
+    
+    tf::TransformListener listener;
+    geometry_msgs::PointStamped pout;
+    geometry_msgs::PointStamped pin;
+    pin.point.x = 0; pin.point.y = 0; pin.point.z = 0;
+    geometry_msgs::Vector3Stamped vout;
+    geometry_msgs::Vector3Stamped vin;
+    vin.vector.x = 0; vin.vector.y = 0; vin.vector.z = 1;
+    
+    ros::Rate rate(0.05); // updating at 5 hz, slightly faster than move_base
+    while (n.ok()) {
+        tf::StampedTransform transform;
+        try {
+            listener.lookupTransform("/base_link", camera_frame, ros::Time(0), transform);
+            transform.transformPoint(camera_frame, ros::Time(0), pin, "/base_link", pout);
+            height = pout.point.z;
+            transform.transformVector3(camera_frame, ros::Time(0), vin, "/base_link", vout);
+            normal = Eigen::Vector3d(vout.vector.x, vout.vector.y, vout.vector.z);
+        }
+        catch (tf::TransformException ex) {
+            ROS_ERROR("%s",ex.what());
+        }
+        rate.sleep();
+        ros::spinOnce();
+    }
+	
+	return 0;
+}
